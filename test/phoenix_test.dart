5e8fc53687a85b11e44fa7a9baa10a9f53e27fdd
lipeng <1162423147@qq.com>
2023-09-21 13:15:40 +0800

    [new] 新建

--- test/phoenix_test.dart
+++ test/phoenix_test.dart
@@ -1,6 +1,14 @@
+/*
+ * @Author: lipeng 1162423147@qq.com
+ * @Date: 2023-09-21 13:04:00
+ * @LastEditors: lipeng 1162423147@qq.com
+ * @LastEditTime: 2023-09-21 13:15:28
+ * @FilePath: /phoenix/test/phoenix_test.dart
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:phoenix/phoenix.dart';
+import 'package:phoenix_base/phoenix.dart';
 
 void main() {
   test('adds one to input values', () {
