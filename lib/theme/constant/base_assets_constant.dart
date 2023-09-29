c7020a649500131d934da1973a225cc212645be9
lipeng <1162423147@qq.com>
2023-09-29 10:05:33 +0800

    [modify] 修改resource 路径

--- lib/theme/constant/base_assets_constant.dart
+++ lib/theme/constant/base_assets_constant.dart
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2023-08-23 09:53:25
- * @LastEditTime: 2023-09-29 09:54:58
+ * @LastEditTime: 2023-09-29 10:04:47
  * @LastEditors: lipeng 1162423147@qq.com
  * @Description: In User Settings Edit
  * @FilePath: /baseui/lib/theme/total/base_assets_constant.dart
@@ -147,39 +147,39 @@ class BaseAsset {
   static const String iconRadioDisableSingleSelected =
       'icons/radio/disable_single_selected.png';
 
-  /// 评价组件--表情
-  static const String iconAppraiseBadUnselected =
-      'assets/images/icon_appraise_bad_unselected.png';
-  static const String iconAppraiseNotGoodUnselected =
-      'assets/images/icon_appraise_not_good_unselected.png';
-  static const String iconAppraiseOkUnselected =
-      'assets/images/icon_appraise_ok_unselected.png';
-  static const String iconAppraiseGoodUnselected =
-      'assets/images/icon_appraise_good_unselected.png';
-  static const String iconAppraiseSurpriseUnselected =
-      'assets/images/icon_appraise_surprise_unselected.png';
+  // /// 评价组件--表情
+  // static const String iconAppraiseBadUnselected =
+  //     'assets/images/icon_appraise_bad_unselected.png';
+  // static const String iconAppraiseNotGoodUnselected =
+  //     'assets/images/icon_appraise_not_good_unselected.png';
+  // static const String iconAppraiseOkUnselected =
+  //     'assets/images/icon_appraise_ok_unselected.png';
+  // static const String iconAppraiseGoodUnselected =
+  //     'assets/images/icon_appraise_good_unselected.png';
+  // static const String iconAppraiseSurpriseUnselected =
+  //     'assets/images/icon_appraise_surprise_unselected.png';
 
-  static const String iconAppraiseBadDefault =
-      'assets/images/icon_appraise_bad_default.png';
-  static const String iconAppraiseNotGoodDefault =
-      'assets/images/icon_appraise_not_good_default.png';
-  static const String iconAppraiseOkDefault =
-      'assets/images/icon_appraise_ok_default.png';
-  static const String iconAppraiseGoodDefault =
-      'assets/images/icon_appraise_good_default.png';
-  static const String iconAppraiseSurpriseDefault =
-      'assets/images/icon_appraise_surprise_default.png';
+  // static const String iconAppraiseBadDefault =
+  //     'assets/images/icon_appraise_bad_default.png';
+  // static const String iconAppraiseNotGoodDefault =
+  //     'assets/images/icon_appraise_not_good_default.png';
+  // static const String iconAppraiseOkDefault =
+  //     'assets/images/icon_appraise_ok_default.png';
+  // static const String iconAppraiseGoodDefault =
+  //     'assets/images/icon_appraise_good_default.png';
+  // static const String iconAppraiseSurpriseDefault =
+  //     'assets/images/icon_appraise_surprise_default.png';
 
-  static const String iconAppraiseBadSelected =
-      'assets/images/icon_appraise_bad_selected.gif';
-  static const String iconAppraiseNotGoodSelected =
-      'assets/images/icon_appraise_not_good_selected.gif';
-  static const String iconAppraiseOkSelected =
-      'assets/images/icon_appraise_ok_selected.gif';
-  static const String iconAppraiseGoodSelected =
-      'assets/images/icon_appraise_good_selected.gif';
-  static const String iconAppraiseSurpriseSelected =
-      'assets/images/icon_appraise_surprise_selected.gif';
+  // static const String iconAppraiseBadSelected =
+  //     'assets/images/icon_appraise_bad_selected.gif';
+  // static const String iconAppraiseNotGoodSelected =
+  //     'assets/images/icon_appraise_not_good_selected.gif';
+  // static const String iconAppraiseOkSelected =
+  //     'assets/images/icon_appraise_ok_selected.gif';
+  // static const String iconAppraiseGoodSelected =
+  //     'assets/images/icon_appraise_good_selected.gif';
+  // static const String iconAppraiseSurpriseSelected =
+  //     'assets/images/icon_appraise_surprise_selected.gif';
   static const String iconQuestion = "icons/icon_question.png";
 
   /// 录入项icon
