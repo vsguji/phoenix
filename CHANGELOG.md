3efbb5047c3b55fd7ac5c637af2745bbaef45278
lipeng <1162423147@qq.com>
2024-02-29 14:40:05 +0800

    add

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,7 +2,7 @@
  * @Author: lipeng 1162423147@qq.com
  * @Date: 2023-09-21 12:57:38
  * @LastEditors: lipeng 1162423147@qq.com
- * @LastEditTime: 2024-02-29 14:34:51
+ * @LastEditTime: 2024-02-29 14:39:56
  * @FilePath: /phoenix/CHANGELOG.md
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -92,8 +92,11 @@
 
 * [新增] phoenix.png
 
-
 ## 1.2.2
+
+* [新增] link color
+
+## 1.2.3
 
 * [新增] link color
 
--- lib/utils/css/css_2_text.dart
+++ lib/utils/css/css_2_text.dart
@@ -2,7 +2,7 @@
  * @Author: lipeng 1162423147@qq.com
  * @Date: 2023-08-31 14:41:55
  * @LastEditors: lipeng 1162423147@qq.com
- * @LastEditTime: 2024-02-29 14:34:31
+ * @LastEditTime: 2024-02-29 14:39:40
  * @FilePath: /phoenix/lib/utils/css/css_2_text.dart
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -34,12 +34,14 @@ class BrnCSS2Text {
     int? maxLines,
     TextAlign? textAlign,
     TextOverflow? textOverflow,
+    Color? linkColor,
   }) {
     return Text.rich(
       toTextSpan(
         htmlContent,
         linksCallback: linksCallback,
         defaultStyle: defaultStyle,
+        linkColor: linkColor
       ),
       maxLines: maxLines,
       textAlign: textAlign,
